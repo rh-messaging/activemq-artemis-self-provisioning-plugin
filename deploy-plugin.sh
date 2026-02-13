0773d3fe5639c39c758aeaf9d1607dcec90eb174
rh-messaging-ci <messaging-infra@redhat.com>
2026-02-13 14:48:38 +0000

    Upgrade AMQ Broker Self Provisioning Plugin container images to 7.14.0.SPP.1.CR1

--- deploy-plugin.sh
+++ deploy-plugin.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 TLS_ENABLED="true"
-DEFAULT_IMAGE="quay.io/arkmq-org/activemq-artemis-self-provisioning-plugin:latest"
+DEFAULT_IMAGE=registry.redhat.io/amq-broker-7/amq-broker-714-self-provisioing-plugin-rhel9@sha256:031764a083cc68bd1a6174fc1639ad794eb43f12e2c25d56117cc1ca6d5d1e07
 PLUGIN_IMAGE=${DEFAULT_IMAGE}
 
 SCRIPT_NAME=$(basename "$0")
