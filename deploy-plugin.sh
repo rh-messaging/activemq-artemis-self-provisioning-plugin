40827ae58ba70388443a3f88d062bec9afc31753
rh-messaging-ci <messaging-infra@redhat.com>
2026-02-13 17:00:59 +0000

    Upgrade AMQ Broker Self Provisioning Plugin container images to 7.14.0.SPP.1.CR1

--- deploy-plugin.sh
+++ deploy-plugin.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 TLS_ENABLED="true"
-DEFAULT_IMAGE=registry.redhat.io/amq-broker-7/amq-broker-714-self-provisioing-plugin-rhel9@sha256:031764a083cc68bd1a6174fc1639ad794eb43f12e2c25d56117cc1ca6d5d1e07
+DEFAULT_IMAGE=registry.redhat.io/amq-broker-7/amq-broker-714-self-provisioning-plugin-rhel9@sha256:906d5ef90cd673e069081722737b8c6a02d0e968536d4832e37b59eb0cd6e8ce
 PLUGIN_IMAGE=${DEFAULT_IMAGE}
 
 SCRIPT_NAME=$(basename "$0")
