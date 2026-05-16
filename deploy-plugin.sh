1bfa7febd8ab422dbde9ff654da2c16271640dc0
rh-messaging-ci <messaging-infra@redhat.com>
2026-05-16 03:20:26 +0000

    RH-PATCH Revert patches

--- deploy-plugin.sh
+++ deploy-plugin.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 TLS_ENABLED="true"
-DEFAULT_IMAGE=registry.redhat.io/amq-broker-8/amq-broker-80-self-provisioning-plugin-rhel9@sha256:395a6ab0540755809093cb7a4ebd6ecdd04b3ab79e5a4d816c6382fc387bef3e
+DEFAULT_IMAGE="quay.io/arkmq-org/activemq-artemis-self-provisioning-plugin:latest"
 PLUGIN_IMAGE=${DEFAULT_IMAGE}
 
 SCRIPT_NAME=$(basename "$0")
